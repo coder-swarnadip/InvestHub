35ef7707de92fba4d6a3c0e74158a855ba9e1364
swarnadip sarkar <swarnadipmng@gmail.com>
2026-08-07 03:40:25 +0530

    update redirect

--- backend/src/utils/cookieOpp.js
+++ backend/src/utils/cookieOpp.js
@@ -1,7 +1,7 @@
 const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
 };
 
